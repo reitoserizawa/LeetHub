d8495b5f18157b24210367cf4399e8a26d28f094
Reito <100802000+reitoserizawa@users.noreply.github.com>
2022-09-23 17:38:28 -0400

    Time: 343 ms, Memory: 46 MB - LeetHub

---
+++ single-number/single-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+
+    nums.sort((a,b) => a- b)
+    let seen = []
+    
+    for (let i = 0; i < nums.length; i++) {
+        
+        if (nums[i] === nums[i+1]) {
+            seen.push(nums[i])
+        } else if (seen.includes(nums[i])) {
+            seen.push(nums[i])
+        } else if (!seen.includes(nums[i]) && nums[i] !== nums[i+1]) {
+            return nums[i]
+        }
+        
+    }
+};
