f998d8b97112e03223f51794b37712997e916296
Reito <100802000+reitoserizawa@users.noreply.github.com>
2022-09-27 20:39:23 -0400

    Time: 100 ms, Memory: 44.2 MB - LeetHub

--- reverse-linked-list/reverse-linked-list.js
+++ reverse-linked-list/reverse-linked-list.js
@@ -17,9 +17,13 @@ var reverseList = function(head) {
     let reversed = null
     
     while (head !== null) {
+//         next is storing the next loop round
         next = head.next
+//         head is now [1 => null] for the first loop (then [2 => 1 => null])
         head.next = reversed
+//         the head is stored in reversed
         reversed = head
+//         head is now assigned from the next var
         head = next
     }
     
